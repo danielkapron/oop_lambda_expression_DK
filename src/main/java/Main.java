de0e4e100004ed05e0dfd7207d4e0947c8c1e8ab
Daniel <kapron.daniel@gmail.com>
2020-05-16 10:36:53 +0200

    oop_lambda v1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
+import model.Role;
+import model.User;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        User u1 = new User();
+        User u2 = new User("Daniel",
+                "Kapron",
+                "kapron.daniel@gmail.com",
+                "dk");
 
+        System.out.println(u2);
 
+        // edycja roli użytkownika
+
+        u2.setRole(Role.ROLE_ADMIN);
+        System.out.println("Nowa rola: " + u2.getRole());
+        System.out.println(u2);
 
 
 
--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -18,7 +18,96 @@ public class User {
 
     // przypisanie uprawnień
 
-    private Role role = new Role.ROLE_USER();
+    private Role role = Role.ROLE_USER;
+
+    public User(){
+
+    }
+
+    public User(String name,
+                String lastName,
+                String email,
+                String password) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    // metoda -> napisowa reprezentacja obiektu
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", registrationDate=" + registrationDate +
+                ", status=" + status +
+                ", role=" + role +
+                '}';
+    }
+
+    // getters & setters - > metody dostępowe do pól klasowych
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
 
 
 
